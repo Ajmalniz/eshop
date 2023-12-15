7dab058c9a0028bdb76e2d2df01bd41b9d1093f7
Ajmalniz <128520849+Ajmalniz@users.noreply.github.com>
2023-12-15 22:41:27 +0500

    page
    new page updated

--- app/all/page.tsx
+++ app/all/page.tsx
@@ -1,34 +1,36 @@
 import Link from "next/link";
 import { simplifiedProduct } from "../interface";
 import { client } from "../lib/sanity";
-import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 
-async function getData(){
-    const query=`*[_type=='product'][0...4] | order(_createdAt asc) {
+async function getData() {
+  const query = `*[_type == "product"] | order(_createdAt asc){
         _id,
+          "imageUrl": image[0].asset->url,
           price,
           name,
-          "slug":slug.current,
-          "categoryName":category->name,
-          "imageUrl":image[0].asset->url
+          "slug": slug.current,
+          "categoryName": category->name
       }`;
-const data = await client.fetch(query);
-return data;
-    }
-    export const dynamic = "force-dynamic";
-export default async function Newest(){
-const data:simplifiedProduct[] = await getData();
-return(
-    <div className="bg-white">
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+export const dynamic = "force-dynamic";
+
+
+
+export default async function All () {
+    const data: simplifiedProduct[] = await getData();
+
+    return (
+      <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 ">
        <div className="flex justify-between items-center">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-            Our Newest product
+            Our All Products 
         </h2>
-        <Link className="text-primary flex items-center gap-x-1" href="/all">
-            See All {" "}<span><Link href="/All"><ArrowRight /></Link></span>
-        </Link>
        </div>
        <div className="mt-6 grid grid-col-1 gap-x-6 gap-y-10 sm:grid-col-2 lg:grid-cols-4 xl:gap-x-8 ">
         {data.map((product)=> (
@@ -61,8 +63,7 @@ return(
         ))}
        </div>
         </div>
-
     </div>
-)
-
+    );
 }
+ 
