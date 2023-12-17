afbdfc742ece594b1be6d4ce560d1d97231bf670
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2023-12-17 11:18:30 +0500

    fix: make product cards clickable, change routes lowercase

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -2,31 +2,31 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import {ShoppingBag} from "lucide-react";
+import { ShoppingBag } from "lucide-react";
 import { useShoppingCart } from "use-shopping-cart";
-export default function Navbar (){
-    const { handleCartClick} = useShoppingCart()
+export default function Navbar() {
+    const { handleCartClick } = useShoppingCart()
     return <div>
-    <header className="mb-8 border-b">
-<div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
-    <Link href="/"> 
-    <h1 className="text-xl md:text-4xl font-bold">
-        Next<span className="text-primary">Commerce</span></h1></Link>
-        <nav className="hidden gap-12 lg:flex 2xl:ml-16">
-        <Link href="/">Home</Link>
-        <Link href="/Men">Men</Link>
-        <Link href="/Women">Women</Link>
-        <Link href="/Teen">Teen</Link>
-        </nav>
-     <div className="flex divide-x border-r sm:border-l">
-        <Button variant={"outline"}
-        onClick={()=>handleCartClick()}
-        className="flex flex-col gap-y-1.5 h-12 w-12 sm:w-20 md:h-24 md:w-24 rounded-none">
-            <ShoppingBag />
-            <span className="hidden text-xs font-semibold text-gray-500 sm:block">Cart</span>
-        </Button>
-        </div>   
-</div>
-    </header>
+        <header className="mb-8 border-b">
+            <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+                <Link href="/">
+                    <h1 className="text-xl md:text-4xl font-bold">
+                        Next<span className="text-primary">Commerce</span></h1></Link>
+                <nav className="hidden gap-12 lg:flex 2xl:ml-16">
+                    <Link href="/">Home</Link>
+                    <Link href="/men">Men</Link>
+                    <Link href="/women">Women</Link>
+                    <Link href="/teen">Teen</Link>
+                </nav>
+                <div className="flex divide-x border-r sm:border-l">
+                    <Button variant={"outline"}
+                        onClick={() => handleCartClick()}
+                        className="flex flex-col gap-y-1.5 h-12 w-12 sm:w-20 md:h-24 md:w-24 rounded-none">
+                        <ShoppingBag />
+                        <span className="hidden text-xs font-semibold text-gray-500 sm:block">Cart</span>
+                    </Button>
+                </div>
+            </div>
+        </header>
     </div>
 }
--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -5,8 +5,8 @@ import { fullProduct } from "@/app/interface";
 import { client } from "@/app/lib/sanity";
 import { Button } from "@/components/ui/button";
 import { Star, Truck } from "lucide-react";
-async function getData(slug:string){
-    const query=`*[_type == "product" && slug.current == "${slug}"][0] {
+async function getData(slug: string) {
+  const query = `*[_type == "product" && slug.current == "${slug}"][0] {
         _id,
           image,
           price,
@@ -15,29 +15,29 @@ async function getData(slug:string){
           "slug": slug.current,
           "categoryName": category->name,
           price_id}`;
-const data = await client.fetch(query);
-return data
-        }
+  const data = await client.fetch(query);
+  return data
+}
 
 
 export const dynamic = "force-dynamic"
 
 export default async function ProductPge({
-    params,
-  }: {
-    params: { slug: string };
-  }){
-    const data:fullProduct = await getData(params.slug)        
-    
-    
-    
-    return(
- 
-        <div className="bg-white">
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data: fullProduct = await getData(params.slug)
+
+
+
+  return (
+
+    <div className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 md:px-8">
         <div className="grid gap-8 md:grid-cols-2">
-        <ImageGallery images={data.image} />
-        <div className="md:py-8">
+          <ImageGallery images={data.image} />
+          <div className="md:py-8">
             <div className="mb-2 md:mb-3">
               <span className="mb-0.5 inline-block text-gray-500">
                 {data.categoryName}
@@ -48,7 +48,7 @@ export default async function ProductPge({
             </div>
             <div className="mb-6 flex items-center gap-3 md:mb-10">
 
-            <Button className="rounded-full gap-x-2">
+              <Button className="rounded-full gap-x-2">
                 <span className="text-sm">4.2</span>
                 <Star className="h-5 w-5" />
               </Button>
@@ -63,41 +63,41 @@ export default async function ProductPge({
                   {data.price}
                 </span>
                 <span className="mb-0.5 text-red-500 line-through">
-                  {data.price + data.price*0.4}
+                  {data.price + data.price * 0.4}
                 </span>
               </div>
               <span className="text-sm text-gray-500">
                 Incl. Vat plus shipping
               </span>
-             </div>
-             <div className="mb-6 flex items-center gap-2 text-gray-500">
+            </div>
+            <div className="mb-6 flex items-center gap-2 text-gray-500">
               <Truck className="w-6 h-6" />
               <span className="text-sm">2-4 Day Shipping</span>
-             </div>
-             <div className="flex gap-2.5">
-                <AddToCart 
+            </div>
+            <div className="flex gap-2.5">
+              <AddToCart
                 currency="Rupees"
                 description={data.description}
-                image={data.image[0]} 
-                name={data.name} 
-                price={data.price}
-                key={data._id}
-                price_id={data.price_id}/>
-                <div ><CheckoutNow
-                currency="Rupees"
-                description={data.description}
-                image={data.image[0]} 
-                name={data.name} 
+                image={data.image[0]}
+                name={data.name}
                 price={data.price}
                 key={data._id}
                 price_id={data.price_id} />
-             </div></div>
-             <p className="mt-12 text-base text-gray-500 tracking-wide">
+              <div ><CheckoutNow
+                currency="Rupees"
+                description={data.description}
+                image={data.image[0]}
+                name={data.name}
+                price={data.price}
+                key={data._id}
+                price_id={data.price_id} />
+              </div></div>
+            <p className="mt-12 text-base text-gray-500 tracking-wide">
               {data.description}
             </p>
-             </div>
+          </div>
         </div>
-        </div>    
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
