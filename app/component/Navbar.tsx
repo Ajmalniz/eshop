0c8b71cd2fcfe2220ba612962c5bb22e5f74c27b
Ajmalniz <128520849+Ajmalniz@users.noreply.github.com>
2023-12-17 23:41:06 +0500

    Update Navbar.tsx

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
                     <h1 className="text-xl md:text-4xl font-bold">
                         Next<span className="text-primary">Commerce</span></h1></Link>
                 <nav className="hidden gap-12 lg:flex 2xl:ml-16">
-                    <Link href="/">Home</Link>
+                    <Link href="/">Home</Link>{/*folders name changed */}
                     <Link href="/men">Men</Link>
                     <Link href="/women">Women</Link>
                     <Link href="/teen">Teen</Link>
